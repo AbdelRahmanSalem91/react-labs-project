a7f6c9d17860f830d13f19eeed19b7963b487531
AbdelRahmanSalem91 <abdoahmed424@gmail.com>
2021-10-20 00:16:09 +0200

    Getting products using fakwstoreapi, and render them in DOM

--- src/components/shop/Shop.jsx
+++ src/components/shop/Shop.jsx
@@ -1,13 +1,47 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { Card } from "react-bootstrap";
 
 const Shop = () => {
-  useEffect(() => {}, []);
-  const getProducts = () => {};
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  const [products, setProducts] = useState([]);
+
+  const getProducts = () => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((json) => setProducts(json));
+  };
 
   return (
-    <>
-      <h2>Shop</h2>
-    </>
+    <div className="product d-flex justify-content-around flex-wrap">
+      {products.map((product) => {
+        return (
+          <Card key={product.id} style={{ width: "30%" }} className="mb-5 py-5">
+            <Card.Img
+              variant="top"
+              src={product.image}
+              className="w-50 mx-auto"
+            />
+            <Card.Body>
+              <Card.Title className="text-nowrap text-truncate">
+                {product.title}
+              </Card.Title>
+              <Card.Text className="text-nowrap text-truncate">
+                {product.description}
+              </Card.Text>
+              <Card.Text>
+                <strong>Price:</strong> {product.price}
+              </Card.Text>
+              <Card.Text>
+                <strong>Rate:</strong> {product.rating.rate}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        );
+      })}
+    </div>
   );
 };
 export default Shop;
